b8c8ee5ed1c6e3f21695c1b298a48431fb681a1a
Gerardo Aguilar Sámano <gerardo.aguilar@dinkbit.com>
2022-09-27 18:58:45 -0500

    Add config form

---
+++ resources/lang/es/forms.php
@@ -0,0 +1,10 @@
+<?php
+ 
+return [
+    'Name'              => 'Nombre',
+    'Email'             => 'Correo electrónico',
+    'Password'          => 'Contraseña',
+    'Confirm Password'  => 'Confirmar contraseña',
+    'Is Active'         => 'Activo',
+    'Save'              => 'Guardar'
+];
